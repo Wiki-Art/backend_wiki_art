5a49a3c0d4e69ddbcf6936597417a15eb080aaba
Thiago Nogueira <thiagonf10@gmail.com>
2020-02-17 16:36:53 -0300

    Fixing travis errors

--- src/utils/models_mixing.py
+++ src/utils/models_mixing.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class InformationMixing(models.Model):
+class InformationMixing (models.Model):
     created = models.DateTimeField(
         editable=False, auto_now_add=True)
     edited = models.DateTimeField(auto_now=True, auto_now_add=False)

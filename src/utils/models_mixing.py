560156cd89c2c132e79b7e78cc7691aabdcb3054
Thiago Nogueira <thiagonf10@gmail.com>
2020-02-17 15:35:00 -0300

    Fixing models InformationMixing

--- src/utils/models_mixing.py
+++ src/utils/models_mixing.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 
-class InformationMixin(models.Model):
-    created = models.DateTimeField(editable=False, blank=True, auto_now_add=True)
-    author_send = models.CharField(blank=False, null=False, max_length=50)
+class InformationMixing(models.Model):
+    created = models.DateTimeField(
+        editable=False, auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True, auto_now_add=False)
+    author_send = models.CharField(blank=False, null=False, max_length=100)
     valid = models.BooleanField(blank=False, null=False, default=False)
-    
+
     class Meta:
         abstract = True

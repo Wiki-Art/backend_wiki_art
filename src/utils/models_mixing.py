a1773e74824f2d25d1e5924138403fb86a2a15ac
Thiago Nogueira <thiagonf10@gmail.com>
2020-02-18 17:21:37 -0300

    Fixing models and choices utils

--- src/utils/models_mixing.py
+++ src/utils/models_mixing.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 
-class InformationMixing (models.Model):
+class TimesTempMixing(models.Model):
     created = models.DateTimeField(
         editable=False, auto_now_add=True)
     edited = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    class Meta:
+        abstract = True
+
+
+class InformationMixing(models.Model):
     author_send = models.CharField(blank=False, null=False, max_length=100)
     valid = models.BooleanField(blank=False, null=False, default=False)
 

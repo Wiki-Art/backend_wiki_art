58356790b1ce3dfc7ce81353ff2d4f6bb73b193b
Thiago Nogueira <thiagonf10@gmail.com>
2020-02-21 14:35:36 -0300

    Adding tabular in line on admin from church app

--- src/church/admin.py
+++ src/church/admin.py
@@ -3,27 +3,15 @@ from django.contrib import admin
 from .models import Church, PictureChurch, ArcticleChurch
 
 
-@admin.register(Church)
-class ChurchAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'created',
-        'edited',
-        'author_send',
-        'valid',
-        'name',
-        'year_foundation',
-        'state',
-        'city',
-    )
-    list_filter = ('created', 'edited', 'valid')
-    search_fields = ('name',)
-
-
 @admin.register(PictureChurch)
 class PictureChurchAdmin(admin.ModelAdmin):
     list_display = ('id', 'created', 'edited', 'church', 'picture')
     list_filter = ('created', 'edited', 'church')
+
+
+class PictureChurchInline(admin.TabularInline):
+    model = PictureChurch
+    extra = 0
 
 
 @admin.register(ArcticleChurch)
@@ -38,3 +26,26 @@ class ArcticleChurchAdmin(admin.ModelAdmin):
         'url',
     )
     list_filter = ('created', 'edited', 'valid', 'church')
+
+
+class ArcticleChurchInline(admin.TabularInline):
+    model = ArcticleChurch
+    extra = 0
+
+
+@admin.register(Church)
+class ChurchAdmin(admin.ModelAdmin):
+    inlines = (PictureChurchInline, ArcticleChurchInline, )
+    list_display = (
+        'id',
+        'created',
+        'edited',
+        'author_send',
+        'valid',
+        'name',
+        'year_foundation',
+        'state',
+        'city',
+    )
+    list_filter = ('created', 'edited', 'valid')
+    search_fields = ('name',)

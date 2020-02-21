47fdc28b7d2630122c9df6ace635f831079cb809
Thiago Nogueira <thiagonf10@gmail.com>
2020-02-21 14:35:23 -0300

    Adding tabular in line on admin from paiting app

--- src/painting/admin.py
+++ src/painting/admin.py
@@ -4,8 +4,39 @@ from django.contrib import admin
 from .models import WorkArt, PictureWorkArt, ArcticleWorkArt
 
 
+class PictureWorkArtInline(admin.TabularInline):
+    model = PictureWorkArt
+    extra = 0
+
+
+@admin.register(PictureWorkArt)
+class PictureWorkArtAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created', 'edited', 'work_art', 'picture')
+    list_filter = ('created', 'edited', 'work_art')
+
+
+class ArcticleWorkArtInline(admin.TabularInline):
+    model = ArcticleWorkArt
+    extra = 0
+
+
+@admin.register(ArcticleWorkArt)
+class ArcticleWorkArtAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'created',
+        'edited',
+        'author_send',
+        'valid',
+        'work_art',
+        'url',
+    )
+    list_filter = ('created', 'edited', 'valid', 'work_art')
+
+
 @admin.register(WorkArt)
 class WorkArtAdmin(admin.ModelAdmin):
+    inlines = (PictureWorkArtInline, ArcticleWorkArtInline,)
     list_display = (
         'id',
         'created',
@@ -21,23 +52,3 @@ class WorkArtAdmin(admin.ModelAdmin):
     )
     list_filter = ('created', 'edited', 'valid', 'author', 'church')
     search_fields = ('name',)
-
-
-@admin.register(PictureWorkArt)
-class PictureWorkArtAdmin(admin.ModelAdmin):
-    list_display = ('id', 'created', 'edited', 'work_art', 'picture')
-    list_filter = ('created', 'edited', 'work_art')
-
-
-@admin.register(ArcticleWorkArt)
-class ArcticleWorkArtAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'created',
-        'edited',
-        'author_send',
-        'valid',
-        'work_art',
-        'url',
-    )
-    list_filter = ('created', 'edited', 'valid', 'work_art')

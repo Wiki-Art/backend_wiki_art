0b1fc48764374aa64c7f706e3761a81f158cb2a8
Thiago Nogueira <thiagonf10@gmail.com>
2020-02-27 17:36:27 -0300

    Adding relay and django-filter in app author

--- src/author/schema.py
+++ src/author/schema.py
@@ -1,25 +1,46 @@
 import graphene
 from graphene_django.types import DjangoObjectType, ObjectType
-from author.models import Author
+from author.models import Author, ArcticleAuthor
+from core.custom_node import CustomNode
+from graphene_django.filter import DjangoFilterConnectionField
+from graphql import GraphQLError
 
 
 class AuthorType(DjangoObjectType):
     class Meta:
         model = Author
-        fields = '__all__'
+        filter_fields = {
+            'name': ['icontains', 'exact'],
+            'race': ['exact'],
+            'state': ['exact'],
+            'city': ['icontains', 'exact'],
+            'created': ['exact'],
+            'edited': ['exact'],
+        }
+        interfaces = (CustomNode,)
+
+
+class ArcticleAuthorType(DjangoObjectType):
+    class Meta:
+        model = ArcticleAuthor
+        filter_fields = {
+            'author': ['exact'],
+            'author_send': ['icontains', 'exact'],
+            'valid': ['exact'],
+            'created': ['exact'],
+            'edited': ['exact'],
+        }
+        interfaces = (CustomNode,)
 
 
 class Query(ObjectType):
-    author = graphene.Field(AuthorType, id=graphene.Int())
-    authors = graphene.List(AuthorType)
+    author = graphene.Field(AuthorType, id=graphene.Int(required=True))
+    authors = DjangoFilterConnectionField(AuthorType)
+    articles_churches = DjangoFilterConnectionField(ArcticleAuthorType)
 
-    def resolve_author(self, info, **kwargs):
-        id = kwargs.get('id', None)
-
-        if id is not None:
+    def resolve_church(self, info, **kwargs):
+        id = kwargs.get('id')
+        try:
             return Author.objects.get(pk=id)
-
-        return None
-
-    def resolve_authors(self, info, **kwargs):
-        return Author.objects.all()
+        except Exception:
+            raise GraphQLError('Author not found')

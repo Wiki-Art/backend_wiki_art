321fe84f4adbf7ea2eccd8276e9650f8aa5fd8e5
Thiago Nogueira <thiagonf10@gmail.com>
2020-02-27 17:36:38 -0300

    Adding relay and django-filter in app church

--- src/church/schema.py
+++ src/church/schema.py
@@ -1,25 +1,64 @@
 import graphene
 from graphene_django.types import DjangoObjectType, ObjectType
-from church.models import Church
+from church.models import Church, ArcticleChurch, PictureChurch
+from graphene_django.filter import DjangoFilterConnectionField
+from graphql import GraphQLError
+from core.custom_node import CustomNode
 
 
 class ChurchType(DjangoObjectType):
     class Meta:
         model = Church
-        fields = '__all__'
+        filter_fields = {
+            'name': ['icontains', 'exact'],
+            'year_foundation': ['exact'],
+            'state': ['exact'],
+            'city': ['icontains', 'exact'],
+            'author_send': ['icontains', 'exact'],
+            'valid': ['exact'],
+            'created': ['exact'],
+            'edited': ['exact'],
+        }
+        interfaces = (CustomNode,)
+
+
+class ArcticleChurchType(DjangoObjectType):
+    class Meta:
+        model = ArcticleChurch
+        filter_fields = {
+            'church__name': ['exact', 'icontains'],
+            'church__state': ['exact'],
+            'church__city': ['icontains', 'exact'],
+            'author_send': ['icontains', 'exact'],
+            'valid': ['exact'],
+            'created': ['exact'],
+            'edited': ['exact'],
+        }
+        interfaces = (CustomNode,)
+
+
+class PictureChurchType(DjangoObjectType):
+    class Meta:
+        model = PictureChurch
+        filter_fields = {
+            'church__name': ['exact', 'icontains'],
+            'church__state': ['exact'],
+            'church__city': ['exact', 'icontains'],
+            'created': ['exact'],
+            'edited': ['exact'],
+        }
+        interfaces = (CustomNode,)
 
 
 class Query(ObjectType):
-    church = graphene.Field(ChurchType, id=graphene.Int())
-    churchs = graphene.List(ChurchType)
+    church = graphene.Field(ChurchType, id=graphene.Int(required=True))
+    churches = DjangoFilterConnectionField(ChurchType)
+    pictures_churches = DjangoFilterConnectionField(PictureChurchType)
+    articles_churches = DjangoFilterConnectionField(ArcticleChurchType)
 
     def resolve_church(self, info, **kwargs):
-        id = kwargs.get('id', None)
-
-        if id is not None:
+        id = kwargs.get('id')
+        try:
             return Church.objects.get(pk=id)
-
-        return None
-
-    def resolve_churchs(self, info, **kwargs):
-        return Church.objects.all()
+        except Exception:
+            raise GraphQLError('Church not found')

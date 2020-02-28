7ab5b7fe00e54f8ffda3f8d84d71953bf7f1ec9c
Thiago Nogueira <thiagonf10@gmail.com>
2020-02-28 15:24:17 -0300

    Fixing url and add mutations in core project

--- src/core/schema.py
+++ src/core/schema.py
@@ -5,25 +5,25 @@ from django.conf import settings
 
 from author.schema import Query as QueryAuthor
 from church.schema import Query as QueryChurch
-# from church.schema import Mutation as MutationChurch
+from church.schema import Mutation as MutationChurch
 from painting.schema import Query as QueryWorkArt
 
 
 class Query(QueryAuthor, QueryChurch, QueryWorkArt, graphene.ObjectType):
+    if settings.DEBUG:
+        # Debug output - seeMost APIs don’t just allow you to read data, they also allow you to write. # noqa
+        # http://docs.graphene-python.org/projects/django/en/latest/debug/
+        debug = graphene.Field(DjangoDebug, name='__debug')
+
+
+class Mutation(MutationChurch, graphene.ObjectType):
     if settings.DEBUG:
         # Debug output - see
         # http://docs.graphene-python.org/projects/django/en/latest/debug/
         debug = graphene.Field(DjangoDebug, name='__debug')
 
 
-# class Mutation(MutationChurch, graphene.ObjectType):
-#     if settings.DEBUG:
-#         # Debug output - see
-#         # http://docs.graphene-python.org/projects/django/en/latest/debug/
-#         debug = graphene.Field(DjangoDebug, name='__debug')
-
-
 schema = graphene.Schema(query=Query,
-                         #  mutation=Mutation,
+                         mutation=Mutation,
                          auto_camelcase=False,
                          )
